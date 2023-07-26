ef0d3b0abaf6e3a2af140d2dd2cb607f9a63e95a
Acuaro <67112172+acuaroo@users.noreply.github.com>
2023-07-26 06:50:39 -0600

    Init

---
+++ data-lerp.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def lerp_chunk(chunk_df, target_rows=15):
+    if len(chunk_df) == target_rows:
+        return chunk_df
+
+    linspace = pd.DataFrame(
+        index=pd.RangeIndex(target_rows),
+        columns=chunk_df.columns
+    )
+
+    linspace["time"] = chunk_df["time"].iloc[0]
+    linspace["is_exploiting"] = chunk_df["is_exploiting"].iloc[0]
+
+    for col in ["x", "y", "z"]:
+        linspace[col] = pd.Series(np.interp(
+            np.linspace(0, 1, num=target_rows),
+            np.linspace(0, 1, num=len(chunk_df)),
+            chunk_df[col]
+        ))
+
+    return linspace
+
+df = pd.read_csv("data/data.csv")
+df.sort_values(by=["time"], inplace=True)
+result_df = df.groupby("time", group_keys=False).apply(lerp_chunk)
+
+result_df.reset_index(drop=True, inplace=True)
+result_df.to_csv("data/lerped-data.csv", index=False)
+
---
+++ data-parser.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import re
+import csv
+
+data = pd.read_csv("data/rawdata.txt", header=None, sep=" ", quoting=csv.QUOTE_NONE, on_bad_lines="skip")
+
+to_replace = {
+    "false": "\nfalse",
+    "true": "\ntrue",
+    "%": "\n"
+}
+
+for value, replace in to_replace.items():
+    drop = True if value == "%" else False
+
+    data[0] = data[0].str.replace(value, replace).str.split("\n")
+    data = data.explode(0).reset_index(drop=drop)
+
+previous_is_exploiting = "false"
+current_time = 0
+
+data.insert(0, "time", "")
+data.insert(1, "is_exploiting", "")
+data.insert(3, "x", "")
+data.insert(4, "y", "")
+data.insert(4, "z", "")
+
+for index, row in data.iterrows():
+    modifier = 0
+
+    if "false" in row[0] or "true" in row[0]:
+        is_exploiting = 0 if "false" in row[0] else 1
+
+        modifier += 1
+        current_time += 1
+        
+        previous_is_exploiting = is_exploiting
+        data.at[index, 0] = data.at[index, 0].replace(f"{is_exploiting},", "")
+
+    data.at[index, "time"] = current_time
+    data.at[index, "is_exploiting"] = previous_is_exploiting
+
+    split = row[0].split(",")
+
+    if len(split) >= 3 + modifier:
+        data.at[index, "x"] = "{:.2f}".format(float(split[0 + modifier]))
+        data.at[index, "y"] = "{:.2f}".format(float(split[1 + modifier]))
+        data.at[index, "z"] = "{:.2f}".format(float(split[2 + modifier]))
+
+data = data.drop(columns=["index", "level_0", 0])
+data.to_csv("data/data.csv", index=False)
+
